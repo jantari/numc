00794760016e1844b3559b2a495ae0525886a157
jantari <jantari@live.com>
2017-02-13 00:21:35 +0100

    better error handling

--- numc.c
+++ numc.c
@@ -13,12 +13,28 @@ int main(int argc, char *argv[])
 	unsigned long long int divergebnis = 1, endzahl2 = 0;
 
 	// Kontrollmechanismen von hier bis Hauptprogramm
-	if (argc < 3) printf("Usage: numc input-number [input number system] target-number-system [-v]\n");
-	else if (strtol(argv[1], NULL, 10) > pow(10,18)-1 || strtol(argv[1], NULL, 10) < 0) printf("\nOnly positive numbers of up to 18 digits are supported currently.");
-	else if (strtol(argv[2], NULL, 10) < 2 || strtol(argv[2], NULL, 10) > 36) printf("\nOnly number systems on base 2 - 36 are supported currently.");
-	else if (argc > 3)
+	if (argc < 3)
 	{
-		if (strcmp(argv[3], "-v") && strtol(argv[3], NULL, 10) < 2 || strtol(argv[3], NULL, 10) > 36) printf("\nOnly number systems on base 2 - 36 are supported currently.");
+		printf("Usage: numc input-number [input number system] target-number-system [-v]\n");
+		return -1;
+	}
+	if (strtol(argv[1], NULL, 10) > pow(10,18)-1 || strtol(argv[1], NULL, 10) < 0)
+	{
+		printf("\nOnly positive numbers of up to 18 digits are supported currently.\n");
+		return -1;
+	}
+	if (strtol(argv[2], NULL, 10) < 2 || strtol(argv[2], NULL, 10) > 36)
+	{
+		printf("\nOnly number systems on base 2 - 36 are supported currently.\n");
+		return -1;
+	}
+	if (argc > 3 && strcmp(argv[3], "-v"))
+	{
+		if (strtol(argv[3], NULL, 10) < 2 || strtol(argv[3], NULL, 10) > 36)
+		{
+			printf("\nOnly number systems on base 2 - 36 are supported currently.\n");
+			return -1;
+		}
 	}
 	// Hauptprogramm
 	for (args = argc - 1; args > 2; args--) if (!strcmp(argv[args], "-v"))
@@ -32,14 +48,16 @@ int main(int argc, char *argv[])
 		// Berechnung andere Zahlensysteme zu Dezimal (Routine 1)
 		for (int position = 0; position != strlen(argv[1]); position++)
 		{
-			if (argv[1][position] != 0) divergebnis = argv[1][position] - 48;
-			endzahl2 = endzahl2 + divergebnis * pow(strtol(argv[2], NULL, 10),strlen(argv[1]) - position - 1);
-			
+			if (argv[1][position] != 48)
+			{
+				if (argv[1][position] > 57) divergebnis = argv[1][position] - 55;
+				else if (argv[1][position] < 58) divergebnis = argv[1][position] - 48;
+				endzahl2 = endzahl2 + divergebnis * pow(strtol(argv[2], NULL, 10),strlen(argv[1]) - position - 1);	
+			}
 			for (args =argc - 1; args > 2; args--) if(!strcmp(argv[args], "-v"))
 			{
 				printf("\n%s hoch %lu: %i",argv[2],strlen(argv[1]) - position - 1,(int)pow(strtol(argv[2], NULL, 10),strlen(argv[1]) - position - 1));
-				printf("\nargv[1][POSITION]: %i",argv[1][position] - 48);
-				printf("\nposition, position -1: %i, %i",position, position -1);
+				printf("\nValue on position %i: %i",position,argv[1][position] - 48);
 				printf("\nENDZAHL2: %llu\n",endzahl2);
 			}
 		}
@@ -68,7 +86,7 @@ int main(int argc, char *argv[])
 			i = i -1;
 			printf("%c", endzahl[i]);
 		} while (i >= 0);
-      	}	
+	}
 	// Programmende
 	printf("\n\n");
 	return 0;
