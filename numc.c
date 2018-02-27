32af276a1ddb74180221e7f5b528334956825dd1
jantari <jantari@live.com>
2018-02-27 14:22:48 -0800

    optimizations
    only calculate strlen of input once instead of many many many times, moved a bunch of variables to be scoped instead of global and cleaned up a logic/syntax or two

--- numc.c
+++ numc.c
@@ -23,8 +23,7 @@ int main(int argc, char *argv[])
 
 	// Using getopt
 	int option = 0;
-	int index;
-	while ((option = getopt(argc, argv, "i:s:t:hv")) != -1) {
+	while ((option = getopt(argc, argv, "s:t:hv")) != -1) {
         switch (option) {
 			case 's':
 				if(!(source_num_sys = atoi(optarg))) {
@@ -51,9 +50,12 @@ int main(int argc, char *argv[])
 	}
 
 	char *input;
-	for (index = optind; index < argc; index++) {
+	int input_length = 0;
+
+	for (int index = optind; index < argc; index++) {
 		if (argv[optind]) {
-			input = malloc(strlen(argv[index]) * sizeof(char) + 1);
+			input_length = strlen(argv[index]);
+			input = malloc(input_length * sizeof(char) + 1);
 			strcpy(input, argv[index]);
 		} else {
 			printf("Please specify a number to convert.");
@@ -61,14 +63,13 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	for (int iterate_through_string = 0; iterate_through_string < strlen(input); iterate_through_string++) {
-		int ia = 0;
-		if (input[iterate_through_string] > '9') {
-			ia = input[iterate_through_string] - '7';
+	for (unsigned int iterate = 0, currentValue = 0; iterate < input_length; iterate++) {
+		if (input[iterate] > '9') {
+			currentValue = input[iterate] - '7';
 		} else {
-			ia = input[iterate_through_string] - '0';
+			currentValue = input[iterate] - '0';
 		}
-		if (ia >= source_num_sys) {
+		if (currentValue >= source_num_sys) {
 			printf("%s is not a valid number in the %i-number system.\n", input, source_num_sys);
 			exit(-1);
 		}
@@ -77,7 +78,7 @@ int main(int argc, char *argv[])
 	// Hauptprogramm
 	if (source_num_sys != 10) {
 		// Berechnung andere Zahlensysteme zu Dezimal (Routine 1)
-		endzahl2 = other_to_decimal(input, source_num_sys, verbosemode);
+		endzahl2 = other_to_decimal(input, input_length, source_num_sys, verbosemode);
 		// Ausgabe 
 		if (verbosemode == 1) printf("\n%s(%d) in decimal is: ", input, source_num_sys);
 		else if (verbosemode == 1) printf("%llu\n",endzahl2);
@@ -98,20 +99,20 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-unsigned long long int other_to_decimal (char input[], int source_num_sys, _Bool verbosemode) {
+unsigned long long int other_to_decimal (char input[], int input_length, int source_num_sys, _Bool verbosemode) {
 	unsigned long long int divergebnis = 1, endzahl2 = 0;
 
-	for (int position = 0; position != strlen(input); position++) {
+	for (int position = 0; position != input_length; position++) {
 		if (input[position] != 48)
 		{
 			if (input[position] > 57) divergebnis = input[position] - 55;
 			else if (input[position] < 58) divergebnis = input[position] - 48;
 
-			endzahl2 += divergebnis * pow(source_num_sys, strlen(input) - position - 1);	
+			endzahl2 += divergebnis * pow(source_num_sys, input_length - position - 1);	
 		}
 		if (verbosemode == 1)
 		{
-			printf("\n%d ^ %lu: %i", source_num_sys, strlen(input) - position - 1, (int)pow(source_num_sys, strlen(input) - position - 1));
+			printf("\n%d ^ %lu: %i", source_num_sys, input_length - position - 1, (int)pow(source_num_sys, input_length - position - 1));
 			printf("\nPosition: %i --- Value: %c", position, input[position]);
 			printf("\n>> Result so far: %llu\n",endzahl2);
 		}
@@ -123,8 +124,9 @@ unsigned long long int other_to_decimal (char input[], int source_num_sys, _Bool
 void decimal_to_other (unsigned long long int input, unsigned int target_num_sys, _Bool verbosemode) {
 	// Allocate memory for a string with enough characters to hold the result of the calculation
 	char *output = malloc((int)(log(input) / log(target_num_sys) + 2) * sizeof(char));
+
 	if (verbosemode == 1) {
-		printf("Allocated memory for %f + 2 chars.\n", (log(input) / log(target_num_sys) ) * sizeof(char));
+		printf("Allocated memory for '%f (rounded down) + 2' chars.\n", (log(input) / log(target_num_sys) ) * sizeof(char));
 	}
 
     int i = 0;
